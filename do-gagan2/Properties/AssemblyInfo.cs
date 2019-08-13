0307c4c111c86e5991ea23b027bf4b872310096b
Kazuyoshi Furuta <furuta@do-gugan.com>
2019-08-13 12:47:16 +0900

    2.0.0

--- do-gagan2/Properties/AssemblyInfo.cs
+++ do-gagan2/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("do-gagan2")]
+[assembly: AssemblyProduct("動画眼2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.99.0.0")]
-[assembly: AssemblyFileVersion("1.99.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
